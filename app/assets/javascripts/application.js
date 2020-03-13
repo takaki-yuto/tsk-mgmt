11ba9064d99090aaf2bfe9cdbc667e70cb4449ce
takaki-yuuto <yutoction0206@gmail.com>
2020-03-14 01:33:44 +0900

    初版

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,57 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require moment
+//= require fullcalendar
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+
+$(function () {
+  // 画面遷移を検知
+  $(document).on('turbolinks:load', function () {
+      // lengthを呼び出すことで、#calendarが存在していた場合はtrueの処理がされ、無い場合はnillを返す
+      if ($('#calendar').length) {
+          function eventCalendar() {
+              return $('#calendar').fullCalendar({
+              });
+          };
+          function clearCalendar() {
+              $('#calendar').html('');
+          };
+
+          $(document).on('turbolinks:load', function () {
+              eventCalendar();
+          });
+          $(document).on('turbolinks:before-cache', clearCalendar);
+
+          $('#calendar').fullCalendar({
+              events: '/events.json',
+              titleFormat: 'YYYY年 M月',
+              dayNamesShort: ['日','月','火','水','木','金','土'],
+              header: {
+                left: 'prev, next, today',
+                center: 'title',
+                right: 'month, agendaWeek, agendaDay, listMonth',
+              },
+              buttonText: {
+                today: '今日',
+                month: '月',
+                week: '週',
+                day: '日',
+              },
+          });
+      }
+  });
+});
