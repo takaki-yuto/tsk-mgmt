945db3c042c725ca82855fbe786ec70ba5c2c5de
takaki-yuuto <yutoction0206@gmail.com>
2020-03-18 16:07:07 +0900

    css適用

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,6 +38,7 @@ $(function () {
 
           $('#calendar').fullCalendar({
               events: '/events.json',
+              timeFormat: "H:mm",
               titleFormat: 'YYYY年 M月',
               dayNamesShort: ['日','月','火','水','木','金','土'],
               header: {
@@ -51,8 +52,29 @@ $(function () {
                 day: '日',
                 list: 'タスクリスト',
               },
-              eventClick: function(event) {
-                console.log(event);
+              dayClick: function (start, end, jsEvent, view) {
+                //クリックした日付情報を取得
+                const year = moment(start).year();
+                const month = moment(start).month()+1; //1月が0のため+1する
+                const day = moment(start).date();
+                //イベント登録のためnewアクションを発火
+                $.ajax({
+                  type: 'GET',
+                  url: "event/new",
+                }).done(function (res) {
+                  //イベント登録用のhtmlを作成
+                  $('.modal-body').html(res);
+                  //イベント登録フォームの日付をクリックした日付とする
+                  $('#event_start_time_1i').val(year);
+                  $('#event_start_time_2i').val(month);
+                  $('#event_start_time_3i').val(day);
+                  //イベント登録フォームのモーダル表示
+                  $('#modal').modal();
+                  // 成功処理
+                }).fail(function (result) {
+                  // 失敗処理
+                  alert('エラーが発生しました。運営に問い合わせてください。')
+                });
               },
           });
       }
